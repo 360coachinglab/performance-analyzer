d8118fecb05ce663cb985282ecaa2bebc76869a0
360coachinglab <info@360coachinglab.ch>
2025-10-15 11:42:15 +0200

    Add files via upload

--- utils/athlete_type.py
+++ utils/athlete_type.py
@@ -3,7 +3,7 @@ def determine_athlete_type(vo2_rel, vlamax, ftp, weight):
         return 'Climber / Ausdauer-Athlet'
     elif vlamax > 0.7:
         return 'Sprinter / Explosivtyp'
-    elif ftp / weight > 5.0:
+    elif ftp / max(weight,1e-9) > 5.0:
         return 'Allrounder / XCO-Athlet'
     else:
         return 'Ausgeglichen'
